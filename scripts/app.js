3cae49b785d1ba48e7f110427d15c3a202ac45ba
Aaron Johnston <charlesaaronj@gmail.com>
2020-10-16 15:24:17 -0500

    save

--- scripts/app.js
+++ scripts/app.js
@@ -1,99 +1,40 @@
-const forecast = new Forecast();
-    
-console.log(forecast);
+const conditions = new Conditions();
 
-
-const cityForm = document.querySelector('form');
-const card = document.querySelector('.card');
+const radioForm = document.querySelector('form');
 const details = document.querySelector('.details');
-const time = document. querySelector('img.time');
-const icon = document.querySelector('.icon img');
-const notFound = document.querySelector('.no');
-
-//4 - update the UI with this function
-const updateUI = (data) => {
-
-  //destructure the data - stores each data type in the same const name
-
-    const {cityDets, weather} = data;
+const time = document. querySelector('.time');
 
 
-details.innerHTML = `
-<h5 class="my-3">${cityDets.EnglishName}</h5>
-<div class="my-3">${weather.WeatherText}</div>
-<div class="display-4 my-4">
-  <span>${weather.Temperature.Imperial.Value}</span>
-  <span>&deg;C</span>
-  `;
-  
-const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
-
-icon.setAttribute('src',iconSrc);
-
-let timeSrc = weather.IsDayTime ? 'img/day.svg' : 'img/night.svg';
-
-// if(weather.IsDayTime){
-//     timeSrc='img/day.svg'
-// } else {
-//     timeSrc='img/night.svg'
-// };
-time.setAttribute('src', timeSrc);
-
-
-if (card.classList.contains('d-none')){
-    card.classList.remove('d-none');
-}
-};
-//2 - takes city value user types in on form
-// const updateCity = async (city) => {
-//     console.log('User searched for city: ' + (city));
-//     //returns promise so use await
-//     //functions in forecast.js - comes before app.js in HTML
-//     const cityDets = await getCity(city);
-//     const weather = await getWeather(cityDets.Key);
-//     //returns object with the city details and weather
-//     console.log('Conditions returned: City Details and Weather')
-//     //use object shorthand to shorten weather: weather
-//     return { cityDets, weather };
-// };
-//1 - add listener to form submit event
-cityForm.addEventListener('submit', e => {
+radioForm.addEventListener('submit', e => {
     e.preventDefault();
-    //get value typed in from 'city' field
-    const city = cityForm.city.value.trim();
-    cityForm.reset();
-
-// 3 - update UI with city typed in
-//getWeather returns a promise so we can use .then
-    forecast.updateCity(city)
+    const resort = document.getElementById('select').value;
+    console.log(resort);
+    radioForm.reset();
+    conditions.getWeather(resort)
     .then(data => {
-        console.log(data);
-        //call updateUI function
         updateUI(data);
-        notFound.style.display = 'none';
-
-        localStorage.setItem('city', city);
     })
     .catch(err => {
         console.log(err);
-        console.log('no location');
-        notFound.style.display = 'block';
-        notFound.style.color = 'red';
-    
     });
-    
-    
 });
 
-// if(localStorage.getItem('city')){
-//     forecast.updateCity(localStorage.getItem('city'))
-//     .then(data => {
-//         updateUI(data)
-//     })
-//     .catch(err => console.log(err));
-
-//     };
-
-
+const updateUI = (data) => {
+    details.innerHTML = `
+    <div>${data.Link}</div>
+    <div>${data.WeatherText}</div>
+    <div>
+      <span>${data.Temperature.Imperial.Value}</span>
+      <span>&deg;C</span>
+      `;
+    
+    if(data.IsDayTime){
+        console.log('Daytime')
+        time.innerHTML = `Daytime`;
+     } else {
+        console.log('Nighttime')
+        time.innerHTML = `Nighttime`;
+     };
+    };
 
 
--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,92 +1,21 @@
-class Forecast {
+class Conditions {
     constructor(){
         this.apiKey = 'mRoY47MbBZ4snv1ND35KgnWXafVI5ImR';
         this.weatherURI = 'https://dataservice.accuweather.com/currentconditions/v1/';
-        this.cityURI = 'https://dataservice.accuweather.com/locations/v1/cities/search';
     }
-    async updateCity(city){
-            console.log('User searched for city: ' + (city));
-            //returns promise so use await
-            //functions in forecast.js - comes before app.js in HTML
-            const cityDets = await this.getCity(city);
-            const weather = await this.getWeather(cityDets.Key);
-            //returns object with the city details and weather
-            console.log('Conditions returned: City Details and Weather')
-            //use object shorthand to shorten weather: weather
-            return { cityDets, weather };
-        }
-        async getCity(city){
-            //added on to end of URL
-            const query = `?apikey=${this.apiKey}&q=${city}`;
-            //fetch base + query URL and returns promise
-            const response = await fetch(this.cityURI + query);
-            //turns reponse into json data and returns promise
-            const data = await response.json();
-            //taking the first element from the array of data (closest match)
-            console.log('API call returns city key associated to city name')
-            return data;
-        }
-        async getWeather(id){
+
+        async getWeather(resort){
     
-            const query = `${id}?apikey=${this.apiKey}`;
+            const query = `${resort}?apikey=${this.apiKey}`;
         
             const response = await fetch(this.weatherURI + query);
         
             const data = await response.json();
+            
             console.log('City key used to return current conditions')
+
             console.log(data);
+
             return data[0];
         }
     }
-
-
-
-// const apiKey = 'mRoY47MbBZ4snv1ND35KgnWXafVI5ImR';
-
-// //get weather - second task to do 
-
-// //take in id of city from getCity function
-// const getWeather = async (id) => {
-
-//     const base = 'https://dataservice.accuweather.com/currentconditions/v1/';
-//     //const base = 'http://dataservice.accuweather.com/forecasts/v1/daily/1day/';
-
-//     const query = `${id}?apikey=${apiKey}`;
-    
-//     const response = await fetch(base + query);
-    
-//     const data = await response.json();
-//     console.log('City key used to return current conditions')
-//     console.log(data);
-//     return data[0];
-
-// };
-
-// // getWeather('331645');
-
-// //1 - get city id with API call
-// const getCity = async (city) => {
-//     //base URL of the API endpoint
-//     const base = 'https://dataservice.accuweather.com/locations/v1/cities/search';
-//     //added on to end of URL
-//     const query = `?apikey=${apiKey}&q=${city}`;
-//     //fetch base + query URL and returns promise
-//     const response = await fetch(base + query);
-//     //turns reponse into json data and returns promise
-//     const data = await response.json();
-// //taking the first element from the array of data (closest match)
-//     console.log('API call returns city key associated to city name')
-//     return data[0];
-    
-// }
-
-// getCity();
-
-
-// getCity('cullman')
-// .then(data => {
-//     return getWeather(data.Key);
-// }).then(data => {
-//     console.log(data);
-// }).catch(err => console.log(err));
-
