27e779d2e01555a38a11e4f0677d5a08c521910c
charlesaaronj <charlesaaronj@gmail.com>
2020-10-18 18:57:33 -0500

    update

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,16 @@
 const conditions = new Conditions();
 
 const radioForm = document.querySelector('form');
-const details = document.querySelector('.details');
+const current = document.querySelector('.current');
 const time = document. querySelector('.time');
+const moreInfo = document. querySelector('.more-info');
+const body = document. querySelector('body');
 
 
 radioForm.addEventListener('submit', e => {
     e.preventDefault();
     const resort = document.getElementById('select').value;
     console.log(resort);
-    radioForm.reset();
     conditions.getWeather(resort)
     .then(data => {
         updateUI(data);
@@ -20,21 +21,31 @@ radioForm.addEventListener('submit', e => {
 });
 
 const updateUI = (data) => {
-    details.innerHTML = `
-    <div>${data.Link}</div>
-    <div>${data.WeatherText}</div>
-    <div>
+    current.innerHTML = `
+    <div class="label mt-5">Current Conditions</div>
+    <div class="is-size-1">
       <span>${data.Temperature.Imperial.Value}</span>
       <span>&deg;C</span>
+      </div>
+    <div class="is-size-5">${data.WeatherText}</div>
       `;
-    
     if(data.IsDayTime){
         console.log('Daytime')
-        time.innerHTML = `Daytime`;
+        body.classList.add("day");
+        body.classList.remove("night");
+        time.innerHTML = `<div class="is-size-5">Day</div>`;
      } else {
         console.log('Nighttime')
-        time.innerHTML = `Nighttime`;
+        body.classList.add("night");
+        body.classList.remove("day");
+        time.innerHTML = `<div class="is-size-5">Night</div>`;
      };
-    };
+    
+    moreInfo.innerHTML = `
+    <div class="is-size-6">
+    <a href="${data.Link}" target="_blank">
+    More Info</a></div>`;
+    
 
+};
 
